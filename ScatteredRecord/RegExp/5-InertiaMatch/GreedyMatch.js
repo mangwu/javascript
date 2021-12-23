cc53f200bcfe71a809824a8ff9043571df9ee685
mangwu <1185956753@qq.com>
2021-12-23 18:43:59 +0800

    greedmatch

---
+++ ScatteredRecord/RegExp/5-InertiaMatch/GreedyMatch.js
@@ -0,0 +1,14 @@
+/**
+ * @description 贪婪性匹配：重复类量词表现未弱贪婪性和强贪婪性
+ * @author mangwu <1185956753@qq.com>
+ * @version  1.0
+ * @date 2021-12-23 13:47:35
+ * @copyright © 2021 wangzhihao, All rights reserved.
+ */
+
+const html = "<html><head><title></title></head><body></body></html>";
+
+const reg = /(<.*>)(<.*>)/;
+
+const a = html.match(reg);
+console.log(a);

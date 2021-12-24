a79f8e4b791764a45b9fe9198b97b4eec082d406
mangwu <1185956753@qq.com>
2021-12-24 17:30:19 +0800

    边界量词

--- ScatteredRecord/RegExp/5-InertiaMatch/GreedyMatch.js
+++ ScatteredRecord/RegExp/5-InertiaMatch/GreedyMatch.js
@@ -6,9 +6,13 @@
  * @copyright © 2021 wangzhihao, All rights reserved.
  */
 
+
 const html = "<html><head><title></title></head><body></body></html>";
 
+// 前面一个尽量匹配多的
 const reg = /(<.*>)(<.*>)/;
 
 const a = html.match(reg);
 console.log(a);
+
+

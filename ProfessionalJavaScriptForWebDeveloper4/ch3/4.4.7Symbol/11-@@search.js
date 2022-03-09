0d036833dee07153d850f9f5593d96254a5115d5
mangwu <1185956753@qq.com>
2022-03-09 19:42:38 +0800

    Symbol.search

---
+++ ProfessionalJavaScriptForWebDeveloper4/ch3/4.4.7Symbol/11-@@search.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 11-@@search.js                                                       *
+ * @Date: 2022-03-09 18:44:46                                                  *
+ * @LastModifiedDate: 2022-03-09 19:41:43                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2022 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+// 所有正则表达式的原型上都有Symbol.search符号属性
+console.log(RegExp.prototype[Symbol.search]);
+console.log("foobarbar".search("bar"));
+
+class BarSearcher {
+  constructor(str) {
+    this.str = str;
+  }
+  static [Symbol.search](target) {
+    return target.indexOf("bar");
+  }
+  [Symbol.search](target) {
+    // 返回自定义的匹配索引
+    return target.indexOf(this.str);
+  }
+}
+
+const foosearch = new BarSearcher("foo");
+console.log("afoobar".search(BarSearcher));
+console.log("afoobar".search(foosearch));

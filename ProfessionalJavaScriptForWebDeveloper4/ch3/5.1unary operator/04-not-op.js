602c23d7d9f05e50b496ee97309cdf123dec65df
mangwu <1185956753@qq.com>
2022-05-24 22:52:04 +0800

    not-op

---
+++ ProfessionalJavaScriptForWebDeveloper4/ch3/5.1unary operator/04-not-op.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 04-not-op.js                                                         *
+ * @Date: 2022-05-24 22:29:57                                                  *
+ * @LastModifiedDate: 2022-05-24 22:30:59                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2022 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+// 按位非
+let num = 123;
+console.log(~num);
+
+// 相当于取负数然后减去1
+let num2 = 123;
+console.log(-num - 1);

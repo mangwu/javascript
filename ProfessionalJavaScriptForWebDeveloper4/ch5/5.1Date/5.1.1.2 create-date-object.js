f1efb1151b7cf232af62e6421fc22b1e1d736cf5
mangwu <1185956753@qq.com>
2022-06-28 00:17:50 +0800

    Date 继承方法

--- ProfessionalJavaScriptForWebDeveloper4/ch5/5.1Date/5.1.1.2 create-date-object.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch5/5.1Date/5.1.1.2 create-date-object.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 5.1.1.2 create-date-object.js                                        *
  * @Date: 2022-06-27 11:11:57                                                  *
- * @LastModifiedDate: 2022-06-27 15:00:47                                      *
+ * @LastModifiedDate: 2022-06-27 22:58:03                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -18,7 +18,7 @@
 const now = new Date();
 console.log(now.toUTCString());
 
-// 日期格式
+// 日期格式, 使用Date.parse()创建时间戳
 // 推荐使用格式 2022-06-27 14:18:22.258 GMT-0700
 
 const date1 = new Date(Date.parse("2022-06-27 00:00:00.000 GMT+0800"));
@@ -36,4 +36,19 @@ console.log(date3.toUTCString());
 const date4 = new Date("2022-06-31 08:00:00.000 GMT+0800");
 console.log(date4.toUTCString());
 
+// 使用UTC()函数创建时间戳
+const date5 = new Date(Date.UTC(2022, 5, 27));
+console.log(date5);
 
+const date6 = new Date(Date.parse("2022-6-27"));
+console.log(date6);
+
+// 隐式调用
+const date7 = new Date(Date.UTC(2022, 0, 1)); // 2022-1-1 00:00:00 GMT+0000
+const date8 = new Date(2022, 0, 1); // 2021-12-31 16:00:00 GMT+0000;
+console.log(date7, date8);
+
+// 使用Date.now()获取当前时间戳
+const date9 = new Date(Date.now());
+console.log(date9.toUTCString());
+console.log(date9 - now);
---
+++ ProfessionalJavaScriptForWebDeveloper4/ch5/5.1Date/5.1.1.2 inherit-function.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 5.1.1.2 function.js                                                  *
+ * @Date: 2022-06-27 23:02:26                                                  *
+ * @LastModifiedDate: 2022-06-27 23:24:44                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2022 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+// Date引用类型继承的方法
+
+// toLocaleString()
+const date = new Date("2012-2-18 14:42:52.111 GMT+0500");
+console.log(date.toLocaleString());
+
+// toString() 带有时区的日期和信息
+console.log(date.toString());
+
+// valueOf() 返回时间戳
+const date2 = new Date("2012-2-19 14:42:52.111 GMT+0500");
+console.log(date2.valueOf());
+console.log(date2 > date);

f743ffc3edb7b5abb1c7614f3a5932d1477fa847
mangwu <1185956753@qq.com>
2022-07-06 17:36:03 +0800

    String方法

--- ProfessionalJavaScriptForWebDeveloper4/ch5/5.3 Wrapped Type/5.3.3 String.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch5/5.3 Wrapped Type/5.3.3 String.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 5.3.3 String.js                                                      *
  * @Date: 2022-07-05 11:21:31                                                  *
- * @LastModifiedDate: 2022-07-05 23:01:37                                      *
+ * @LastModifiedDate: 2022-07-06 17:32:43                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -74,4 +74,80 @@ console.log(String.fromCharCode(0x0041, 0x030a));
 let a = "Å";
 let b = "Å";
 let c = "Å";
+console.log(a == b, b == c, a == c);
 console.log(a.charCodeAt(), b.charCodeAt(), c.codePointAt());
+
+// normalize
+
+console.log(
+  a.normalize("NFD").charCodeAt(),
+  a.normalize("NFC").charCodeAt(),
+  a.normalize("NFKD").charCodeAt(),
+  a.normalize("NFKC").charCodeAt()
+);
+
+console.log(
+  b.normalize("NFD").charCodeAt(),
+  b.normalize("NFC").charCodeAt(),
+  b.normalize("NFKD").charCodeAt(),
+  b.normalize("NFKC").charCodeAt()
+);
+
+console.log(
+  c.normalize("NFD").charCodeAt(),
+  c.normalize("NFC").charCodeAt(),
+  c.normalize("NFKD").charCodeAt(),
+  c.normalize("NFKC").charCodeAt()
+);
+
+console.log(b == b.normalize("NFC"), b == b.normalize("NFD"));
+
+console.log(
+  a.normalize("NFD") == b.normalize("NFD"),
+  a.normalize("NFKD") == c.normalize("NFKD"),
+  b.normalize("NFC") == c.normalize("NFC")
+);
+
+// 操作字符串的方法
+// concat
+let strValue = "hello ";
+let res = strValue.concat("world");
+console.log(res);
+
+// slice substring substr
+
+let str = "ABCDEFGHIJKLM";
+console.log(str.slice(2, 5), str.substring(2, 5), str.slice(8), str.slice());
+console.log(str.substr(2, 5));
+console.log(str.slice(-8, -2), str.substring(-5, 11), str.substr(-2, 1));
+// indexOf
+let str2 =
+  "Hello, cat, how constructor pop you Oh, you are fine cat, be careful cat";
+console.log(str2.indexOf("cat"));
+let pos = str2.indexOf("cat");
+let allIdx = [];
+while (pos > -1) {
+  allIdx.push(pos);
+  pos = str2.indexOf("cat", pos + 1);
+}
+console.log(allIdx);
+
+// lastIndexOf
+console.log(str2.lastIndexOf("cat"));
+console.log(str2.lastIndexOf("cat", 20));
+
+// includes
+let str3 = "foobarbaz";
+console.log(str3.includes("bar"));
+console.log(str3.includes("bar", 4));
+// startWith
+console.log(str3.startsWith("foo"), str3.startsWith("bar"));
+console.log(str3.startsWith("foo", 3), str3.startsWith("bar", 3));
+// endWith
+console.log(str3.endsWith("baz"), str3.endsWith("bar"));
+console.log(str3.endsWith("baz", 6), str3.endsWith("bar", 6));
+
+// trim()
+let str4 = " \r\n\tabcde  f\r\n\tg hijklmn  \r\n\t";
+console.log(str4, str4.length);
+console.log(str4.trim());

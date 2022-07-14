3b0dd4a2a854364e6631629084fa0bccfedec4fa
mangwu <1185956753@qq.com>
2022-07-15 00:04:57 +0800

    创建数组

--- ProfessionalJavaScriptForWebDeveloper4/ch6 - Collection reference type/6.1 Object/6.1.1 Object.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch6 - Collection reference type/6.1 Object/6.1.1 Object.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 6.1.1 Object.js                                                      *
  * @Date: 2022-07-14 16:37:07                                                  *
- * @LastModifiedDate: 2022-07-14 17:28:40                                      *
+ * @LastModifiedDate: 2022-07-14 22:12:21                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -21,7 +21,7 @@ obj.age = 22;
 // 对象字面量
 let sym = Symbol.iterator;
 const person = {
-  [sym]: function* () {
+  [Symbol.iterator]: function* () {
     yield 1;
     yield 2;
     yield 3;
@@ -41,3 +41,17 @@ function displayInfo(args) {
   return name + age;
 }
 displayInfo({ name: "mangwu", age: 22 });
+
+// 点语法
+person.a = 3;
+// 中括号语法
+console.log(person[5]);
+console.log(person["5"]);
+console.log(person[Symbol.iterator]);
+console.log(person[sym]);
+// 包含特殊字符的字符串属性
+let person2 = {
+  "first name": "mang",
+};
+// 只能通过中括号语法调用
+console.log(person2["first name"]);
---
+++ ProfessionalJavaScriptForWebDeveloper4/ch6 - Collection reference type/6.2 Array/6.2.1 create Array.js
@@ -0,0 +1,68 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 6.2.1 create Array.js                                                *
+ * @Date: 2022-07-14 22:53:22                                                  *
+ * @LastModifiedDate: 2022-07-15 00:03:21                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2022 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+// 创建数组
+const colors = new Array(20);
+console.log(colors, colors.length);
+
+const names = new Array(-3, "a", "b", "c");
+console.log(names);
+
+console.log(Array(2, 5, 6));
+
+// 数组字面量
+console.log([1, 2, , 5]);
+
+// Array.from()
+const set = new Set().add(5).add(1).add(6).add(-2);
+const map = new Map().set(1, 5).set("2", 3).set("m", 8).set("as", 99).set(1, 3);
+const arrayLikeObj = {
+  0: "1",
+  2: "3",
+  5: "6",
+  name: "mangwu",
+  length: 3,
+};
+const iteratorObj = {
+  [Symbol.iterator]: function* () {
+    yield 2;
+    yield 3;
+    yield 5;
+  },
+};
+console.log(Array.from(set));
+console.log(Array.from(map));
+console.log(Array.from(arrayLikeObj));
+console.log(Array.from(iteratorObj));
+
+// 浅复制
+const arr = [[1, 2], 3, { 4: "a", 5: [6] }];
+const copy = Array.from(arr);
+copy[0][0] = "newVal";
+copy[1] = "newVal";
+copy[2][4] = "newVal";
+copy[2][5][0] = "newVal";
+copy.push("newVal");
+console.log(arr);
+console.log(copy);
+
+// argments
+function getArgsArray() {
+  for(const item of arguments) {
+    console.log(item);
+  }
+  return Array.from(arguments);
+}
+
+getArgsArray(1, 2, 3, 4);

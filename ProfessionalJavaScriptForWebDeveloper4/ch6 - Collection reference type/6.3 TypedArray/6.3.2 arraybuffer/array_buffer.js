6d0440ab00038e6a5ebb6ce2e129bdeefaacfdb8
mangwu <1185956753@qq.com>
2022-07-27 12:43:20 +0800

    arraybuffer

--- ProfessionalJavaScriptForWebDeveloper4/ch6 - Collection reference type/6.3 TypedArray/6.3.2 arraybuffer/array_buffer.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch6 - Collection reference type/6.3 TypedArray/6.3.2 arraybuffer/array_buffer.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: array_buffer.js                                                      *
  * @Date: 2022-07-26 17:30:49                                                  *
- * @LastModifiedDate: 2022-07-26 17:30:58                                      *
+ * @LastModifiedDate: 2022-07-27 12:43:02                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -12,5 +12,9 @@
  * ---------------------	--------	----------------------------------------------- *
  */
 
-
-// 类型数组
+// 类型数组 ArrayBuffer 创建通用的、固定长度的原始二进制数据缓冲区
+const ab = new ArrayBuffer(5);
+console.log(ab);
+console.log(ab.byteLength);
+console.log(ArrayBuffer.length);
+console.log(ArrayBuffer.isView(ab));

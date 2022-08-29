5dd278af0db14e64a0e0fc599733f3f5e17188d3
mangwu <1185956753@qq.com>
2022-08-29 23:46:47 +0800

    reduce

--- ProfessionalJavaScriptForWebDeveloper4/ch6 - Collection reference type/6.3 TypedArray/6.3.4 ElementArray/6.3.4.3 ElementTypeArrayMethod.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch6 - Collection reference type/6.3 TypedArray/6.3.4 ElementArray/6.3.4.3 ElementTypeArrayMethod.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 6.3.4.3 ElementTypeArrayMethod.js                                    *
  * @Date: 2022-08-26 10:38:20                                                  *
- * @LastModifiedDate: 2022-08-26 15:15:21                                      *
+ * @LastModifiedDate: 2022-08-29 22:35:04                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -57,3 +57,23 @@ console.log(new Uint16Array([NaN]).includes(NaN));
 console.log(new Float32Array([NaN]).includes(NaN));
 console.log(new Uint16Array([NaN]));
 console.log(new Float32Array([NaN]));
+
+// 11. indexOf()
+console.log(new Float32Array([NaN]).indexOf(NaN));
+
+// 12 lastIndexOf
+console.log(new Float32Array([NaN]).lastIndexOf(NaN));
+
+// 13. join()
+console.log(int32Arr.join());
+
+// 14 keys()
+for (let key of int32Arr.keys()) {
+  console.log(key, typeof key);
+}
+
+// 15 map
+console.log([1, , 2, 3, ,].map((v) => 2));
+console.log(new Int32Array(5).map((_v) => 5));
+
+// 16 reduce / reduceRight

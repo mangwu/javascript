1aadd8498ff8b93ee1b6ab5f9f16514f00486811
mangwu <1185956753@qq.com>
2022-09-01 23:31:11 +0800

    Xset\

--- ProfessionalJavaScriptForWebDeveloper4/ch6 - Collection reference type/6.6 Set/6.6.3 XSet/6.6.3 XSet.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch6 - Collection reference type/6.6 Set/6.6.3 XSet/6.6.3 XSet.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 6.5.6 XSet.js                                                        *
  * @Date: 2022-09-01 11:21:19                                                  *
- * @LastModifiedDate: 2022-09-01 15:24:26                                      *
+ * @LastModifiedDate: 2022-09-01 20:17:43                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -63,4 +63,38 @@ class XSet extends Set {
   symmetricDifference(setb) {
     return XSet.symmetricDifference(this, setb);
   }
+  static cartesianProduct(seta, setb) {
+    const cb = [];
+    for (const vala of seta) {
+      for (const valb of setb) {
+        cb.push([vala, valb]);
+      }
+    }
+    return cb;
+  }
+  cartesianProduct(setb) {
+    return XSet.cartesianProduct(this, setb);
+  }
+  static powerSet(set) {
+    // 初始保存空集
+    const powerSet = new XSet().add(new XSet());
+    for (const addVal of set) {
+      for (const addSet of new XSet(powerSet)) {
+        powerSet.add(new XSet(addSet).add(addVal));
+      }
+    }
+    return powerSet;
+  }
+  powerSet() {
+    return XSet.powerSet(this);
+  }
 }
+
+const xseta = new XSet([1, 3, 5, 7]);
+const xsetb = new XSet([3, 4, 7]);
+console.log(xseta.union(xsetb)); // 并集
+console.log(xseta.intersection(xsetb)); // 交集
+console.log(xseta.difference(xsetb)); // 差集
+console.log(xseta.symmetricDifference(xsetb)); // 对称差集
+console.log(xseta.cartesianProduct(xsetb)); // 笛卡尔积
+console.log(xseta.powerSet()); // 幂集

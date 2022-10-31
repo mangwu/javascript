ee4d41a91121ab341fd416ee2253ff780b0fa757
mangwu <1185956753@qq.com>
2022-11-01 02:34:33 +0800

    chapter03

---
+++ JavaScriptDataStructuresAndAlgorithms3/useful/chapter02/lib3/2.2.9.1 create module.js
@@ -0,0 +1,48 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.squareArea = exports.default = exports.circleArea = void 0;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+/*
+ * @Author: mangwu                                                             *
+ * @File: 2.2.8.1 create module.js                                             *
+ * @Date: 2022-10-31 16:17:02                                                  *
+ * @LastModifiedDate: 2022-10-31 16:49:12                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2022 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+// 创建模块
+var circleArea = function circleArea(r) {
+  return Math.PI * Math.pow(r, 2);
+};
+exports.circleArea = circleArea;
+var squareArea = function squareArea(s) {
+  return s * s;
+};
+exports.squareArea = squareArea;
+// 这就是创建的一个模块，可被其它文件使用
+// 导出默认值
+var Book = /*#__PURE__*/function () {
+  function Book(title) {
+    _classCallCheck(this, Book);
+    this.title = title;
+  }
+  _createClass(Book, [{
+    key: "printTitle",
+    value: function printTitle() {
+      console.log(this.title);
+    }
+  }]);
+  return Book;
+}();
+exports.default = Book;
---
+++ JavaScriptDataStructuresAndAlgorithms3/useful/chapter02/lib3/2.2.9.2 import module.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+require("core-js/modules/es.array.iterator.js");
+require("core-js/modules/es.object.to-string.js");
+require("core-js/modules/es.string.iterator.js");
+require("core-js/modules/es.weak-map.js");
+require("core-js/modules/web.dom-collections.iterator.js");
+require("core-js/modules/es.object.get-own-property-descriptor.js");
+require("core-js/modules/es.symbol.js");
+require("core-js/modules/es.symbol.description.js");
+require("core-js/modules/es.symbol.iterator.js");
+var area = _interopRequireWildcard(require("./2.2.9.1 create module.js"));
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+/*
+ * @Author: mangwu                                                             *
+ * @File: 2.2.9.2 import module.js                                             *
+ * @Date: 2022-10-31 16:36:17                                                  *
+ * @LastModifiedDate: 2022-10-31 21:45:03                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2022 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+// 引入模块
+
+console.log((0, area.circleArea)(5));
+console.log((0, area.squareArea)(3));
+console.log(area.circleArea(8));
+console.log(area.squareArea(8));
+console.log(area);
+
+// 引入默认模块
+// 自定义默认模块的名称
+
+var myBook = new area.default("some title");
+myBook.printTitle();

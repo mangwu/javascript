53d0cc629a579d1ed8860dd22be6dc4204bbf998
mangwu <1185956753@qq.com>
2022-11-15 01:17:50 +0800

    setTimeout

--- ProfessionalJavaScriptForWebDeveloper4/ch12 - BOM/12.1 window/12.1.7 window.setTimeout window.setInterval/12.1.7.1 window.setTimeout/12.1.7.1 MDN-understand window.setTimeout/12.1.7.1.3 functionRef this problem.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch12 - BOM/12.1 window/12.1.7 window.setTimeout window.setInterval/12.1.7.1 window.setTimeout/12.1.7.1 MDN-understand window.setTimeout/12.1.7.1.3 functionRef this problem.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 12.1.7.1.3 .js                                                       *
  * @Date: 2022-11-14 16:56:25                                                  *
- * @LastModifiedDate: 2022-11-14 17:35:18                                      *
+ * @LastModifiedDate: 2022-11-14 22:34:03                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2022 mangwu                                                   *
@@ -14,24 +14,29 @@
 
 // 计时器函数的this默认为全局对象
 globalThis.a = "a";
-// const obj = {
-//   a: "objA",
-//   method() {
-//     console.log(this);
-//     console.log(this.a);
-//   },
-// };
-// obj.method();
-// setTimeout(obj.method);
-
-// setTimeout.call(obj, obj.method);
-
-// 解决方案
-const obj2 = {
-  a: "obj2A",
-  method: () => {
+const obj = {
+  a: "objA",
+  method() {
     console.log(this);
     console.log(this.a);
   },
 };
-setTimeout.call(obj2, obj2.method);
+obj.method();
+setTimeout(obj.method);
+
+setTimeout.call(obj, obj.method);
+
+// 解决方案
+// 使用箭头函数
+function A() {
+  this.a = "funcA";
+  setTimeout(() => {
+    console.log(this.a);
+  });
+}
+A();
+
+setTimeout(function () {
+  obj.method();
+});
+setTimeout(obj.method.bind(obj))

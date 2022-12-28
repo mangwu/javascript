48cd20167b9dbe2105f54eeb44f0150a9db3f2d7
mangwu <1185956753@qq.com>
2022-12-28 17:45:41 +0800

    元素节点

---
+++ ProfessionalJavaScriptForWebDeveloper4/ch14 - DOM/14.1 HIERARCHY OF NODES/14.1.2 Document Type/14.1.2.6 script.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 14.1.2.6 script.js                                                   *
+ * @Date: 2022-12-28 13:34:16                                                  *
+ * @LastModifiedDate: 2022-12-28 13:34:44                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2022 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+console.log("Hello, world!");

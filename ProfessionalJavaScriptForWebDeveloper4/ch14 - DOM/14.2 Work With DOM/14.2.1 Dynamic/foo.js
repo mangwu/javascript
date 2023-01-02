106def4d16bb9f2017a6facc3ce8a59894ed5b7e
mangwu <1185956753@qq.com>
2023-01-03 04:46:35 +0800

    动态样式

---
+++ ProfessionalJavaScriptForWebDeveloper4/ch14 - DOM/14.2 Work With DOM/14.2.1 Dynamic/foo.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: foo.js                                                               *
+ * @Date: 2023-01-01 09:32:27                                                  *
+ * @LastModifiedDate: 2023-01-01 09:32:41                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2023 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+console.log("hello, world");

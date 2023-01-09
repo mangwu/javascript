a148eb8274885588b0c6900a6b18f0021c88d464
mangwu <1185956753@qq.com>
2023-01-10 02:33:57 +0800

    scrollIntoView

--- ProfessionalJavaScriptForWebDeveloper4/ch15 - DOM Extensions/15.3 HTML5/15.3.6 Insertion Markup/15.3.6.6 scrollIntoView.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch15 - DOM Extensions/15.3 HTML5/15.3.6 Insertion Markup/15.3.6.6 scrollIntoView.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 15.3.6.6 scrollIntoView.js                                           *
  * @Date: 2023-01-09 15:52:45                                                  *
- * @LastModifiedDate: 2023-01-09 16:15:42                                      *
+ * @LastModifiedDate: 2023-01-09 22:20:35                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2023 mangwu                                                   *
@@ -12,11 +12,10 @@
  * ---------------------	--------	----------------------------------------------- *
  */
 
-let btn = document.querySelector(".btn");
 let el = document.querySelector(".special");
 
-btn.addEventListener("click", function () {
-  el.scrollIntoView(true, {
+document.body.addEventListener("click", function () {
+  el.scrollIntoView({
     behavior: "smooth",
     block: "center",
     inline: "center",

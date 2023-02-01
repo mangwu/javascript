ddd6e6f81f34ca5c07942310899fb47d75102e18
mangwu <1185956753@qq.com>
2023-02-01 17:33:04 +0800

    deleteContents

--- ProfessionalJavaScriptForWebDeveloper4/ch16 - DOM Levels 2 and 3/16.4 Range/16.4.1 example/16.4.1.0 supplement/16.4.1.0.2 selector content.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch16 - DOM Levels 2 and 3/16.4 Range/16.4.1 example/16.4.1.0 supplement/16.4.1.0.2 selector content.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 16.4.1.0.2 selector content.js                                       *
  * @Date: 2023-01-31 16:53:11                                                  *
- * @LastModifiedDate: 2023-01-31 17:04:10                                      *
+ * @LastModifiedDate: 2023-02-01 10:48:09                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2023 mangwu                                                   *
@@ -17,3 +17,8 @@ const articleContent = document.querySelector(".section-content");
 const p = articleContent.children[1];
 range.setStart(p, 3);
 range.setEnd(p.childNodes[10], 15);
+console.log(range);
+console.log(range.toString());
+
+// range.deleteContents();
+// console.log(range);

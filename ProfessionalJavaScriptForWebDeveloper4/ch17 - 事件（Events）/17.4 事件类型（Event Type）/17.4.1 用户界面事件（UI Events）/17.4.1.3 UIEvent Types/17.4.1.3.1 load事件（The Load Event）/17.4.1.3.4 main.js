624d43a9810ada4232c6ecdae65cb3610dc23707
mangwu <1185956753@qq.com>
2023-02-13 17:35:31 +0800

    load

---
+++ ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.1 用户界面事件（UI Events）/17.4.1.3 UIEvent Types/17.4.1.3.1 load事件（The Load Event）/17.4.1.3.4 main.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 17.4.1.3.4 main.js                                                   *
+ * @Date: 2023-02-13 17:08:42                                                  *
+ * @LastModifiedDate: 2023-02-13 17:08:55                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2023 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+console.log("Hello, world!");

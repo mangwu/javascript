18af18a145a1a5bc7b7eb8d98a529eab0a0088e3
mangwu <1185956753@qq.com>
2023-03-06 01:58:57 +0800

    mouseout

---
+++ ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.1 The Mouse Event/17.4.3.1.2 鼠标事件类型（Mouse Event Type）/17.4.3.1.2.9 mouseout/17.4.3.1.2.9 mouseout.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 17.4.3.1.2.2 mouseup.js                                              *
+ * @Date: 2023-03-02 20:40:56                                                  *
+ * @LastModifiedDate: 2023-03-06 01:04:09                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2023 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+const A = document.querySelector(".A");
+const B = document.querySelector(".B");
+const log = document.querySelector(".log");
+
+function handler(e) {
+  const li = document.createElement("li");
+  li.className = "log-li";
+  li.innerHTML = `<span class="type">${e.type}</span>
+  <span class="target">${e.target.className}</span>
+  <span class="currentTarget">${
+    e.currentTarget && e.currentTarget.className
+  }</span>
+  <span class="base">${e.detail}</span>
+  <span class="base">${e.screenX}/${e.screenY}</span>
+  <span class="base">${e.clientX}/${e.clientY}</span>
+  <span class="base">${e.ctrlKey}</span>
+  <span class="base">${e.button}</span>
+  <span class="base">${e.buttons}</span>
+  <span class="base">${e.relatedTarget.className}</span>
+  `;
+  log.appendChild(li);
+}
+
+// A.addEventListener("mouseover", handler);
+A.addEventListener("mouseout", handler);
+
+// B.addEventListener("mouseover", handler);
+B.addEventListener("mouseout", handler);

96194f9c4fb66eeebd763a5dd70ea548af16fcce
mangwu <1185956753@qq.com>
2023-03-09 17:56:10 +0800

    wheel event

--- ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.2 The Wheel Event/17.4.3.2.1 define/17.4.3.2.1 wheel event type.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.2 The Wheel Event/17.4.3.2.1 define/17.4.3.2.1 wheel event type.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 17.4.3.2.1 wheel event type.js                                       *
  * @Date: 2023-03-09 00:30:12                                                  *
- * @LastModifiedDate: 2023-03-09 00:39:16                                      *
+ * @LastModifiedDate: 2023-03-09 09:14:48                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2023 mangwu                                                   *
@@ -15,6 +15,7 @@
 const div = document.querySelector("div");
 
 div.addEventListener("wheel", (e) => {
+  console.log(e);
   div.innerHTML = `<div class="text">wheelEvent.ctrlKey: <span>${e.ctrlKey}</span>, 
   wheelEvent.shiftKey: <span>${e.shiftKey}</span></div>`;
 });

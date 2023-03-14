5f1ecf337719dba151b43a54962db3acff475dcb
mangwu <1185956753@qq.com>
2023-03-14 17:32:23 +0800

    processing

--- ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.3 The Drag Event/17.4.3.3.5 Processing model/17.4.3.3.5.1 processing/17.4.3.3.5.1 processing.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.3 The Drag Event/17.4.3.3.5 Processing model/17.4.3.3.5.1 processing/17.4.3.3.5.1 processing.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 17.4.3.3.5.1 processing.js                                           *
  * @Date: 2023-03-14 10:27:04                                                  *
- * @LastModifiedDate: 2023-03-14 11:22:38                                      *
+ * @LastModifiedDate: 2023-03-14 17:31:48                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2023 mangwu                                                   *
@@ -15,15 +15,9 @@
 const divs = document.querySelectorAll(".box");
 const item = document.querySelector(".item");
 const log = document.querySelector(".log");
-item.addEventListener("dragstart", (e) => {
-  if (e.target instanceof HTMLDivElement) {
-    e.dataTransfer.effectAllowed = "move";
-    handler(e);
-  }
-});
-item.addEventListener("dragend", (e) => {
-  handler(e);
-});
+const img = document.createElement("img");
+img.src = "./iconfont-logo.svg";
+
 function debounce(func, delay = 100) {
   let timer = null;
   return function (...args) {
@@ -39,9 +33,6 @@ const debounceHandler = debounce((e) => {
 });
 
 function handler(e) {
-  if (e.type.indexOf("drag") === -1) {
-    console.log(e);
-  }
   const li = document.createElement("li");
   li.className = "log-li";
   li.innerHTML = `<span class="type">${e.type}</span>
@@ -52,41 +43,78 @@ function handler(e) {
   <span class="base">${e.detail}</span>
   <span class="base">${e.screenX}/${e.screenY}</span>
   <span class="base">${e.clientX}/${e.clientY}</span>
-  <span class="base">${e.ctrlKey}</span>
+  <span class="base">${e.ctrlKey}</span>  
   <span class="base">${e.button}</span>
   <span class="base">${e.buttons}</span>
   <span class="relatedTarget">${
     e.relatedTarget && e.relatedTarget.className
   }</span>
+  <span class="relatedTarget">${
+    e.dataTransfer && e.dataTransfer.effectAllowed
+  }</span>
+  <span class="relatedTarget">${
+    e.dataTransfer && e.dataTransfer.dropEffect
+  }</span>
+
   `;
   log.appendChild(li);
 }
-
-divs.forEach((v) => {
+const internalDNDType = "text"; // 指定数据类型
+item.addEventListener("drag", (e) => {
+  e.preventDefault(); // 会设置当前拖动操作为none
+  e.target.style.opacity = 0.5;
+  handler(e); // 开始拖动后，在源节点上第一个激发的拖动事件
+});
+divs.forEach((v, i) => {
+  v.addEventListener("dragstart", (e) => {
+    if (e.target instanceof HTMLDivElement && e.target.className === "item") {
+      e.dataTransfer.effectAllowed = "move"; // 设置允许的拖拽操作
+      e.dataTransfer.setDragImage(img, 40, 40); // 设置UI反馈
+      e.dataTransfer.setData(internalDNDType, i); // 索引
+      handler(e);
+    } else {
+      e.preventDefault(); // 对不是item的div元素直接取消拖拽操作
+    }
+  });
   v.addEventListener("drag", (e) => {
-    handler(e);
+    e.preventDefault();
   });
   v.addEventListener("dragenter", (e) => {
-    e.preventDefault();
     handler(e);
+    // 对进入元素进行样式修改
+    if (e.target === v) {
+      e.target.style.backgroundColor = "red";
+    }
   });
   v.addEventListener("dragleave", (e) => {
-    e.preventDefault();
     handler(e);
+    // 对离开元素进行样式修改
+    if (e.target === v && e.relatedTarget !== item) {
+      e.target.style.backgroundColor = "rgb(231, 116, 116)";
+    }
   });
   v.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    // debounceHandler(e);
-    handler(e);
-  });
-  v.addEventListener("dragout", (e) => {
-    e.preventDefault();
-    // debounceHandler(e);
+    e.dataTransfer.dropEffect = "move";
+    e.preventDefault(); // prevent default to allow drop
     handler(e);
   });
   v.addEventListener("drop", (e) => {
     handler(e);
-    item.parentNode.removeChild(item);
-    e.currentTarget.appendChild(item);
+    // prevent default action (open as link for some elements)
+    e.preventDefault();
+    if (e.currentTarget.dataset.dropzone === "true") {
+      const index = item.parentNode.removeChild(item);
+      e.currentTarget.appendChild(item);
+    }
   });
+  v.addEventListener("dragend", (e) => {
+    if (e.dataTransfer.dropEffect === "move") {
+      e.currentTarget.style.backgroundColor = "rgb(231, 116, 116)";
+      handler(e);
+      e.target.style.opacity = 1;
+    }
+  });
+});
+item.addEventListener("pointercancel", (e) => {
+  handler(e);
 });

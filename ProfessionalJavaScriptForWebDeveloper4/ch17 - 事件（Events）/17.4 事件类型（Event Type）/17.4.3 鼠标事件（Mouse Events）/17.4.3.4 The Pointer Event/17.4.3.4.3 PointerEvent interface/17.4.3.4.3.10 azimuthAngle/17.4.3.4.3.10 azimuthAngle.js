945e60266b18217a44f29b196905f23a1b2eb16b
mangwu <1185956753@qq.com>
2023-03-20 23:59:38 +0800

    pointertype

---
+++ ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.4 The Pointer Event/17.4.3.4.3 PointerEvent interface/17.4.3.4.3.10 azimuthAngle/17.4.3.4.3.10 azimuthAngle.js
@@ -0,0 +1,93 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 17.43.4.3.5 tangential pressure.js                                   *
+ * @Date: 2023-03-19 19:30:12                                                  *
+ * @LastModifiedDate: 2023-03-20 23:16:41                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2023 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+const { useRef, useEffect, useState } = React;
+const { Table, Button } = antd;
+
+const columns = [
+  {
+    dataIndex: "type",
+    key: "type",
+    title: "类型",
+    width: 45,
+  },
+  {
+    dataIndex: "target",
+    key: "target",
+    title: "事件目标",
+    width: 90,
+  },
+  {
+    dataIndex: "currentTarget",
+    key: "currentTarget",
+    title: "当前事件目标",
+    width: 135,
+  },
+  {
+    dataIndex: "pointerId",
+    key: "pointerId",
+    title: "pointerId",
+    width: 67,
+  },
+  {
+    dataIndex: "azimuthAngle",
+    key: "azimuthAngle",
+    title: "azimuthAngle",
+  },
+];
+
+function App() {
+  const [data, setData] = useState([]);
+  const handlePointerClick = (e) => {
+    console.log(e);
+    const { type, currentTarget, pointerId, azimuthAngle, buttons } = e;
+    setData((state) => [
+      ...state,
+      {
+        type,
+        target: e.target.classList[0],
+        currentTarget: currentTarget.classList[0],
+        pointerId,
+        azimuthAngle,
+        buttons,
+        date: new Date().getTime(),
+      },
+    ]);
+  };
+  return (
+    <div className={"container"}>
+      <Button
+        className="click"
+        type="primary"
+        onPointerDown={handlePointerClick}
+        onPointerUp={handlePointerClick}
+        size={"large"}
+      >
+        click me
+      </Button>
+      <Table
+        className={"my-ant-table"}
+        dataSource={data}
+        size={"large"}
+        rowKey={"date"}
+        columns={columns}
+        scroll={{ x: "100%" }}
+        pagination={{ pageSize: 5 }}
+        width={500}
+      />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));

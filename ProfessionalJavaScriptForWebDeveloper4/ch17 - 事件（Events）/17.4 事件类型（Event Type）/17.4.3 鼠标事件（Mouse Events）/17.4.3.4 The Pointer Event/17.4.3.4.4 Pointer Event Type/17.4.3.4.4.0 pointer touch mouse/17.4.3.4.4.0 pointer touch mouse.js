0268ef3a14bb76ac87ba50aaa75b5730ea7e3022
mangwu <1185956753@qq.com>
2023-03-24 17:34:56 +0800

    pointer

---
+++ ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.4 The Pointer Event/17.4.3.4.4 Pointer Event Type/17.4.3.4.4.0 pointer touch mouse/17.4.3.4.4.0 pointer touch mouse.js
@@ -0,0 +1,108 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 17.4.3.4.4.1 pointerover.js                                          *
+ * @Date: 2023-03-23 22:06:57                                                  *
+ * @LastModifiedDate: 2023-03-24 11:26:12                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2023 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+const log = document.querySelector(".log");
+
+TouchList.prototype.map = function (callback) {
+  const len = this.length;
+  let res = [];
+  for (let i = 0; i < len; i++) {
+    res.push(callback(this[i], i, this));
+  }
+  return res;
+};
+
+function handler(e) {
+  const item = document.createElement("div");
+  item.className = "event-info";
+  item.innerHTML = `<span class="base">事件类型：${e.type}</span>
+    <span class="base">事件目标：${e.target.className}</span>
+    <span class="base">当前事件目标：${
+      e.currentTarget && e.currentTarget.className
+    }</span>
+    <span class="base">detail：${e.detail}</span>
+  `;
+  if (e instanceof TouchEvent) {
+    console.log(e.touches);
+    console.log(e.targetTouches);
+    item.innerHTML += `
+      <span class="base">接触点：${e.touches
+        .map(
+          (v) =>
+            `${v.target.className}(${v.clientX.toFixed(2)}
+            /${v.clientY.toFixed(2)})`
+        )
+        .join(" ")}</span>
+      <span class="base">接触目标：${e.targetTouches
+        .map(
+          (v) =>
+            `${v.target.className}(${v.clientX.toFixed(2)}
+          /${v.clientY.toFixed(2)})`
+        )
+        .join(" ")}</span>
+  `;
+  }
+  if (e instanceof MouseEvent) {
+    item.innerHTML += `
+      <span class="base">次要事件目标：${
+        e.relatedTarget && e.relatedTarget.className
+      }</span>
+      <span class="base">屏幕位置：${e.screenX.toFixed(2)}/${e.screenY.toFixed(
+      2
+    )}</span>
+      <span class="base">客户端位置：${e.clientX.toFixed(
+        2
+      )}/${e.clientY.toFixed(2)}</span>
+      <span class="base">引发按键：${e.button}</span>
+      <span class="base">按键状态：${e.buttons}</span>
+    `;
+  }
+  if (e instanceof PointerEvent) {
+    item.innerHTML += `<span class="base">pointerId：${e.pointerId}</span>
+    <span class="base">主指针：${e.isPrimary}</span>
+    <span class="base">倾斜角度：${e.tiltX}/${e.tiltY}</span>
+    <span class="base">压力：${e.pressure}</span>
+    <span class="base">切向压力：${e.tangentialPressure}</span>
+    <span class="base">扭动度：${e.twist}</span>
+    <span class="base">俯仰角：${e.altitudeAngle}</span>
+    <span class="base">方位角：${e.azimuthAngle}</span>
+    `;
+  }
+  log.appendChild(item);
+}
+
+const a = document.querySelector(".A");
+
+a.addEventListener("pointerdown", handler);
+a.addEventListener("pointerup", handler);
+a.addEventListener("pointercancel", handler);
+a.addEventListener("pointermove", handler);
+a.addEventListener("pointerenter", handler);
+a.addEventListener("pointerleave", handler);
+a.addEventListener("pointerover", handler);
+a.addEventListener("pointerout", handler);
+a.addEventListener("click", handler);
+
+a.addEventListener("mousedown", handler);
+a.addEventListener("mouseup", handler);
+a.addEventListener("mousemove", handler);
+a.addEventListener("mouseenter", handler);
+a.addEventListener("mouseleave", handler);
+a.addEventListener("mouseover", handler);
+a.addEventListener("mouseout", handler);
+
+a.addEventListener("touchstart", handler);
+a.addEventListener("touchmove", handler);
+a.addEventListener("touchend", handler);
+a.addEventListener("touchcancel", handler);
--- ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.4 The Pointer Event/17.4.3.4.4 Pointer Event Type/17.4.3.4.4.2 pointerenter/17.4.3.4.4.2 pointerenter.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.4 The Pointer Event/17.4.3.4.4 Pointer Event Type/17.4.3.4.4.2 pointerenter/17.4.3.4.4.2 pointerenter.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 17.4.3.4.4.1 pointerover.js                                          *
  * @Date: 2023-03-23 22:06:57                                                  *
- * @LastModifiedDate: 2023-03-24 09:58:10                                      *
+ * @LastModifiedDate: 2023-03-24 13:31:33                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2023 mangwu                                                   *
@@ -14,24 +14,59 @@
 
 const log = document.querySelector(".log");
 
+TouchList.prototype.map = function (callback) {
+  const len = this.length;
+  let res = [];
+  for (let i = 0; i < len; i++) {
+    res.push(callback(this[i], i, this));
+  }
+  return res;
+};
+
 function handler(e) {
   const item = document.createElement("div");
   item.className = "event-info";
   item.innerHTML = `<span class="base">事件类型：${e.type}</span>
-  <span class="base">事件目标：${e.target.className}</span>
-  <span class="base">当前事件目标：${
-    e.currentTarget && e.currentTarget.className
-  }</span>
-  <span class="base">次要事件目标：${
-    e.relatedTarget && e.relatedTarget.className
-  }</span>
-  <span class="base">detail：${e.detail}</span>
-  <span class="base">屏幕位置：${e.screenX.toFixed(2)}/${e.screenY.toFixed(2)}</span>
-  <span class="base">客户端位置：${e.clientX.toFixed(2)}/${e.clientY.toFixed(2)}</span>
-  <span class="base">引发按键：${e.button}</span>
-  <span class="base">按键状态：${e.buttons}</span>
+    <span class="base">事件目标：${e.target.className}</span>
+    <span class="base">当前事件目标：${
+      e.currentTarget && e.currentTarget.className
+    }</span>
+    <span class="base">detail：${e.detail}</span>
   `;
-  if (e.type.indexOf("pointer") !== -1) {
+  if (e instanceof TouchEvent) {
+    item.innerHTML += `
+      <span class="base">接触点：${e.touches
+        .map(
+          (v) =>
+            `${v.target.className}(${v.clientX.toFixed(2)}
+            /${v.clientY.toFixed(2)})`
+        )
+        .join(" ")}</span>
+      <span class="base">接触目标：${e.targetTouches
+        .map(
+          (v) =>
+            `${v.target.className}(${v.clientX.toFixed(2)}
+          /${v.clientY.toFixed(2)})`
+        )
+        .join(" ")}</span>
+  `;
+  }
+  if (e instanceof MouseEvent) {
+    item.innerHTML += `
+      <span class="base">次要事件目标：${
+        e.relatedTarget && e.relatedTarget.className
+      }</span>
+      <span class="base">屏幕位置：${e.screenX.toFixed(2)}/${e.screenY.toFixed(
+      2
+    )}</span>
+      <span class="base">客户端位置：${e.clientX.toFixed(
+        2
+      )}/${e.clientY.toFixed(2)}</span>
+      <span class="base">引发按键：${e.button}</span>
+      <span class="base">按键状态：${e.buttons}</span>
+    `;
+  }
+  if (e instanceof PointerEvent) {
     item.innerHTML += `<span class="base">pointerId：${e.pointerId}</span>
     <span class="base">主指针：${e.isPrimary}</span>
     <span class="base">倾斜角度：${e.tiltX}/${e.tiltY}</span>
@@ -47,12 +82,12 @@ function handler(e) {
 
 const a = document.querySelector(".A");
 
-a.addEventListener("pointerdown", handler);
-a.addEventListener("pointerover", handler);
-a.addEventListener("mousedown", handler);
-a.addEventListener("mouseover", handler);
-a.addEventListener("click", handler);
+a.addEventListener("pointerenter", handler);
+a.addEventListener("mouseenter", handler);
 
+a.addEventListener("pointerdown", handler);
+a.addEventListener("mousedown", handler);
+a.addEventListener("click", handler);
 
 
 

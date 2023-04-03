ab1f82edaceb186a7324d00bbfd476e1687370c4
mangwu <1185956753@qq.com>
2023-04-04 01:16:09 +0800

    合并事件

--- ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.4 The Pointer Event/17.4.3.4.10 Coalesced Events/17.4.3.4.10.1.1 example10.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.4 The Pointer Event/17.4.3.4.10 Coalesced Events/17.4.3.4.10.1.1 example10.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: 17.4.3.4.10.1.1 example10.js                                         *
  * @Date: 2023-04-01 00:14:22                                                  *
- * @LastModifiedDate: 2023-04-01 00:36:26                                      *
+ * @LastModifiedDate: 2023-04-03 23:14:42                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2023 mangwu                                                   *
@@ -14,9 +14,20 @@
 
 const canvas = document.querySelector("#drawSurface");
 const rect = canvas.getBoundingClientRect();
-console.log(rect);
 const context = canvas.getContext("2d");
+const devicePixelRatio = window.devicePixelRatio || 1;
+canvas.width = rect.width * devicePixelRatio;
+canvas.height = rect.height * devicePixelRatio;
+const inputColor = document.querySelector("input[name='color']");
+const inputRange = document.querySelector("input[name='range']");
 
+inputColor.addEventListener("change", (e) => {
+  context.fillStyle = e.target.value;
+});
+let thickness = 1;
+inputRange.addEventListener("change", (e) => {
+  thickness = e.target.value;
+});
 canvas.addEventListener("pointermove", (e) => {
   if (e.getCoalescedEvents) {
     // 能力检测
@@ -30,8 +41,11 @@ canvas.addEventListener("pointermove", (e) => {
 function paint(e) {
   if (e.buttons > 0) {
     // 按下按键状态x
-    context.fillRect((e.clientX * 3) / 5, (e.clientY * 3) / 10, 5, 5);
-    // 在位置上绘制5 * 5 大小黑色框
+    const canvasRect = canvas.getBoundingClientRect();
+    const x = e.clientX - canvasRect.left;
+    console.log(e.clientX);
+    const y = e.clientY - canvasRect.top;
+    context.fillRect(x * devicePixelRatio, y * devicePixelRatio, thickness, thickness);
   }
 }
 // 500 500

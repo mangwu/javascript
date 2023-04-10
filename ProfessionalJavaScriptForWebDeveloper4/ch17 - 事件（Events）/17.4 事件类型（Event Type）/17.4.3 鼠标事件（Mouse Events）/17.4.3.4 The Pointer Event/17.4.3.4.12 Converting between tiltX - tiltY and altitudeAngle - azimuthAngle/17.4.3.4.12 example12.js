ef45db89c8877c08049e24aec2c2d0526d701c11
mangwu <1185956753@qq.com>
2023-04-10 17:41:03 +0800

    example12

---
+++ ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.3 鼠标事件（Mouse Events）/17.4.3.4 The Pointer Event/17.4.3.4.12 Converting between tiltX - tiltY and altitudeAngle - azimuthAngle/17.4.3.4.12 example12.js
@@ -0,0 +1,125 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 17.4.3.4.12 example12.js                                             *
+ * @Date: 2023-04-10 17:36:40                                                  *
+ * @LastModifiedDate: 2023-04-10 17:40:25                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2023 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+/* Converting between tiltX/tiltY and altitudeAngle/azimuthAngle */
+
+/**
+ * @description 将弧度的俯仰角和高度角转换为度数的倾斜角 
+ * @param {number} altitudeAngle 俯仰角
+ * @param {number} azimuthAngle 高度角
+ * @returns {{titlX, titlY}}
+ */
+function spherical2tilt(altitudeAngle, azimuthAngle) {
+  const radToDeg = 180 / Math.PI; // 弧度和度的比
+
+  let tiltXrad = 0; // 保存X倾斜角弧度
+  let tiltYrad = 0; // 保存Y倾斜角弧度
+
+  if (altitudeAngle == 0) {
+    // the pen is in the X-Y plane
+    if (azimuthAngle == 0 || azimuthAngle == 2 * Math.PI) {
+      // pen is on positive X axis
+      tiltXrad = Math.PI / 2;
+    }
+    if (azimuthAngle == Math.PI / 2) {
+      // pen is on positive Y axis
+      tiltYrad = Math.PI / 2;
+    }
+    if (azimuthAngle == Math.PI) {
+      // pen is on negative X axis
+      tiltXrad = -Math.PI / 2;
+    }
+    if (azimuthAngle == (3 * Math.PI) / 2) {
+      // pen is on negative Y axis
+      tiltYrad = -Math.PI / 2;
+    }
+    if (azimuthAngle > 0 && azimuthAngle < Math.PI / 2) {
+      tiltXrad = Math.PI / 2;
+      tiltYrad = Math.PI / 2;
+    }
+    if (azimuthAngle > Math.PI / 2 && azimuthAngle < Math.PI) {
+      tiltXrad = -Math.PI / 2;
+      tiltYrad = Math.PI / 2;
+    }
+    if (azimuthAngle > Math.PI && azimuthAngle < (3 * Math.PI) / 2) {
+      tiltXrad = -Math.PI / 2;
+      tiltYrad = -Math.PI / 2;
+    }
+    if (azimuthAngle > (3 * Math.PI) / 2 && azimuthAngle < 2 * Math.PI) {
+      tiltXrad = Math.PI / 2;
+      tiltYrad = -Math.PI / 2;
+    }
+  }
+
+  if (altitudeAngle != 0) {
+    const tanAlt = Math.tan(altitudeAngle);
+
+    tiltXrad = Math.atan(Math.cos(azimuthAngle) / tanAlt);
+    tiltYrad = Math.atan(Math.sin(azimuthAngle) / tanAlt);
+  }
+
+  return { tiltX: tiltXrad * radToDeg, tiltY: tiltYrad * radToDeg };
+}
+
+function tilt2spherical(tiltX, tiltY) {
+  const tiltXrad = (tiltX * Math.PI) / 180;
+  const tiltYrad = (tiltY * Math.PI) / 180;
+
+  // calculate azimuth angle
+  let azimuthAngle = 0;
+
+  if (tiltX == 0) {
+    if (tiltY > 0) {
+      azimuthAngle = Math.PI / 2;
+    } else if (tiltY < 0) {
+      azimuthAngle = (3 * Math.PI) / 2;
+    }
+  } else if (tiltY == 0) {
+    if (tiltX < 0) {
+      azimuthAngle = Math.PI;
+    }
+  } else if (Math.abs(tiltX) == 90 || Math.abs(tiltY) == 90) {
+    // not enough information to calculate azimuth
+    azimuthAngle = 0;
+  } else {
+    // Non-boundary case: neither tiltX nor tiltY is equal to 0 or +-90
+    const tanX = Math.tan(tiltXrad);
+    const tanY = Math.tan(tiltYrad);
+
+    azimuthAngle = Math.atan2(tanY, tanX);
+    if (azimuthAngle < 0) {
+      azimuthAngle += 2 * Math.PI;
+    }
+  }
+
+  // calculate altitude angle
+  let altitudeAngle = 0;
+
+  if (Math.abs(tiltX) == 90 || Math.abs(tiltY) == 90) {
+    altitudeAngle = 0;
+  } else if (tiltX == 0) {
+    altitudeAngle = Math.PI / 2 - Math.abs(tiltYrad);
+  } else if (tiltY == 0) {
+    altitudeAngle = Math.PI / 2 - Math.abs(tiltXrad);
+  } else {
+    // Non-boundary case: neither tiltX nor tiltY is equal to 0 or +-90
+    altitudeAngle = Math.atan(
+      1.0 /
+        Math.sqrt(
+          Math.pow(Math.tan(tiltXrad), 2) + Math.pow(Math.tan(tiltYrad), 2)
+        )
+    );
+  }
+
+  return { altitudeAngle: altitudeAngle, azimuthAngle: azimuthAngle };
+}

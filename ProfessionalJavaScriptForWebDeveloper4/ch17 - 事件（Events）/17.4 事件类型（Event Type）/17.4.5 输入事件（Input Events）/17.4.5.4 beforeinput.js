c43afe63d9657b4a0a25a616f412d915c62aad42
mangwu <1185956753@qq.com>
2023-04-17 00:36:25 +0800

    input event

---
+++ ProfessionalJavaScriptForWebDeveloper4/ch17 - 事件（Events）/17.4 事件类型（Event Type）/17.4.5 输入事件（Input Events）/17.4.5.4 beforeinput.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: mangwu                                                             *
+ * @File: 17.4.5.4 .js                                                         *
+ * @Date: 2023-04-16 22:56:45                                                  *
+ * @LastModifiedDate: 2023-04-17 00:35:44                                      *
+ * @ModifiedBy: mangwu                                                         *
+ * -----------------------                                                     *
+ * Copyright (c) 2023 mangwu                                                   *
+ * -----------------------                                                     *
+ * @HISTORY:                                                                   *
+ * Date   	            By 	    Comments                                       *
+ * ---------------------	--------	----------------------------------------------- *
+ */
+
+const input = document.querySelector(".input");
+
+const log = document.querySelector(".log");
+const eventLog = document.querySelector(".event-log");
+
+input.addEventListener("beforeinput", (e) => {
+  log.textContent = e.target.textContent;
+  console.log(e.target.textContent, e.target);
+  const div = document.createElement("div");
+  eventLog.style.border = "1px solid gray";
+  div.className = "row";
+  div.innerHTML = `\
+  <span class="tag">type:${e.type}</span>
+  <span class="tag">inputType:${e.inputType}</span>
+  <span class="tag">data:${e.data}</span>
+  <span class="tag">type:${e.isComposing}</span>
+  `;
+  eventLog.appendChild(div);
+});

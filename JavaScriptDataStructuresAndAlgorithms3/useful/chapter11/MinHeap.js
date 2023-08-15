e954b61ac2e90a4912ec2e000a3f8000b5c286ab
mangwu <1185956753@qq.com>
2023-08-15 10:34:20 +0800

    MinHeap

--- JavaScriptDataStructuresAndAlgorithms3/useful/chapter11/MinHeap.js
+++ JavaScriptDataStructuresAndAlgorithms3/useful/chapter11/MinHeap.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: MinHeap.js                                                           *
  * @Date: 2023-08-14 13:59:10                                                  *
- * @LastModifiedDate: 2023-08-14 17:15:09                                      *
+ * @LastModifiedDate: 2023-08-15 10:28:19                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2023 mangwu                                                   *
@@ -12,10 +12,94 @@
  * ---------------------	--------	----------------------------------------------- *
  */
 
+// class MinHeap {
+//   constructor(compareFn = (a, b) => a - b) {
+//     this.compareFn = compareFn;
+//     this.heap = [];
+//   }
+//   getLeftIndex(idx) {
+//     return 2 * idx + 1;
+//   }
+//   getRightIndex(idx) {
+//     return 2 * idx + 2;
+//   }
+//   getParentIndex(idx) {
+//     if (idx === 0) return -1;
+//     return Math.floor((idx - 1) / 2);
+//   }
+//   insert(value) {
+//     if (value !== null) {
+//       this.heap.push(value);
+//       this.shiftUp(this.heap.length - 1);
+//       return true;
+//     }
+//     return false;
+//   }
+//   shiftUp(idx) {
+//     let parent = this.getParentIndex(idx);
+//     while (idx > 0 && this.compareFn(this.heap[parent], this.heap[idx]) > 0) {
+//       // 父节点的值比当前节点大，需要交换
+//       this.swap(idx, parent);
+//       idx = parent;
+//       parent = this.getParentIndex(idx);
+//     }
+//   }
+//   swap(a, b) {
+//     const temp = this.heap[a];
+//     this.heap[a] = this.heap[b];
+//     this.heap[b] = temp;
+//   }
+//   size() {
+//     return this.heap.length;
+//   }
+//   isEmpty() {
+//     return this.size() === 0;
+//   }
+//   peek() {
+//     return this.isEmpty() ? undefined : this.heap[0];
+//   }
+//   extract() {
+//     if (this.isEmpty()) return undefined;
+//     if (this.size() === 1) return this.heap.pop();
+//     this.swap(0, this.size() - 1);
+//     let removeValue = this.heap.pop();
+//     this.shiftDown(0);
+//     return removeValue;
+//   }
+//   shiftDown(idx) {
+//     let left = this.getLeftIndex(idx);
+//     let right = this.getRightIndex(idx);
+//     let temp = idx;
+//     const size = this.size();
+//     if (left < size && this.compareFn(this.heap[temp], this.heap[left]) > 0) {
+//       // 当前节点值比左子节点大，需要交换
+//       temp = left;
+//     }
+//     if (right < size && this.compareFn(this.heap[temp], this.heap[right]) > 0) {
+//       // 当前节点值比右子节点大，需要交换
+//       temp = right;
+//     }
+//     if (temp !== idx) {
+//       this.swap(temp, idx);
+//       this.shiftDown(temp);
+//     }
+//   }
+// }
 class MinHeap {
   constructor(compareFn = (a, b) => a - b) {
     this.compareFn = compareFn;
     this.heap = [];
+  }
+  swap(a, b) {
+    const temp = this.heap[a];
+    this.heap[a] = this.heap[b];
+    this.heap[b] = temp;
+  }
+  size() {
+    return this.heap.length;
+  }
+  isEmpty() {
+    return this.size() === 0;
   }
   getLeftIndex(idx) {
     return 2 * idx + 1;
@@ -35,7 +119,8 @@ class MinHeap {
     }
     return false;
   }
-  shiftUp(idx) {
+  shiftUp() {
+    let idx = this.size() - 1;
     let parent = this.getParentIndex(idx);
     while (idx > 0 && this.compareFn(this.heap[parent], this.heap[idx]) > 0) {
       // 父节点的值比当前节点大，需要交换
@@ -44,19 +129,40 @@ class MinHeap {
       parent = this.getParentIndex(idx);
     }
   }
-  swap(a, b) {
-    const temp = this.heap[a];
-    this.heap[a] = this.heap[b];
-    this.heap[b] = temp;
-  }
-  size() {
-    return this.heap.length;
-  }
-  isEmpty() {
-    return this.size() === 0;
-  }
   peek() {
     return this.isEmpty() ? undefined : this.heap[0];
+  }
+  poll() {
+    if (this.isEmpty()) return undefined;
+    if (this.size() === 1) return this.heap.pop();
+    this.swap(0, this.size() - 1);
+    let removeValue = this.heap.pop();
+    this.shiftDown();
+    return removeValue;
+  }
+  shiftDown() {
+    let idx = 0;
+    let temp = idx; // 保存被交换的父节点索引
+    const size = this.size();
+    while (idx < size) {
+      let left = this.getLeftIndex(idx);
+      let right = this.getRightIndex(idx);
+      if (left < size && this.compareFn(this.heap[idx], this.heap[left]) > 0) {
+        // 当前节点值比左子节点大，需要交换
+        idx = left;
+      }
+      if (
+        right < size &&
+        this.compareFn(this.heap[idx], this.heap[right]) > 0
+      ) {
+        // 当前节点值比右子节点大，需要交换
+        idx = right;
+      }
+      if (temp !== idx) {
+        this.swap(temp, idx);
+        temp = idx;
+      } else break; // 没有可以交换的节点
+    }
   }
 }
 
--- JavaScriptDataStructuresAndAlgorithms3/useful/chapter11/useMinHeap.js
+++ JavaScriptDataStructuresAndAlgorithms3/useful/chapter11/useMinHeap.js
@@ -2,7 +2,7 @@
  * @Author: mangwu                                                             *
  * @File: useMinHeap.js                                                        *
  * @Date: 2023-08-14 14:51:50                                                  *
- * @LastModifiedDate: 2023-08-14 14:54:15                                      *
+ * @LastModifiedDate: 2023-08-15 10:29:16                                      *
  * @ModifiedBy: mangwu                                                         *
  * -----------------------                                                     *
  * Copyright (c) 2023 mangwu                                                   *
@@ -19,4 +19,31 @@ heap.insert(4);
 heap.insert(3);
 heap.insert(5);
 heap.insert(1);
+
+console.log("min:", heap.peek());
+
+heap.insert(11);
+heap.insert(1);
+heap.insert(8);
+heap.insert(7);
+heap.insert(-7);
+heap.insert(-6);
+heap.insert(-8);
+heap.insert(0);
+console.log("min:", heap.peek());
 console.log(heap.heap);
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+console.log("pop min", heap.poll());
+
+

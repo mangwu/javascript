3c7cfb0a1cb9bdbc6b02b928a8fd762be2b508c8
mangwu <1185956753@qq.com>
2024-06-06 17:42:34 +0800

    performance throttling with requestAnimationFrame

--- ProfessionalJavaScriptForWebDeveloper4/ch18 - Animation and Graphics with Canvas/18.1 使用requestAnimationFrame（Using requestAnimationFrame）/18.1.5 通过requestAnimationFrame节流/18.1.5 Performance Throttling with requestAnimationFrame.js
+++ ProfessionalJavaScriptForWebDeveloper4/ch18 - Animation and Graphics with Canvas/18.1 使用requestAnimationFrame（Using requestAnimationFrame）/18.1.5 通过requestAnimationFrame节流/18.1.5 Performance Throttling with requestAnimationFrame.js
@@ -3,9 +3,49 @@ const scrollWrapper = document.querySelector(".scroll-wrapper");
 let preTime;
 
 scrollWrapper.addEventListener("scroll", (e) => {
-  if (!preTime) preTime = performance.now();
   const time = performance.now();
-  console.log(time - preTime);
+  if (preTime) console.log(time - preTime);
   preTime = time;
+  setTimeout(() => {
+    preTime = null;
+  }, 100);
 });
 
+const scrollThrottlingWrapper = document.querySelector(
+  ".scroll-throttling-wrapper"
+);
+
+const sectionWrappers = document.querySelectorAll(".section-wrapper");
+let sum = 0;
+const sectionLen = [];
+for (const sectionWrapper of sectionWrappers) {
+  sum += sectionWrapper.clientHeight;
+  sectionLen.push(sum);
+}
+const tocParts = document.querySelectorAll(".toc-part");
+
+let ticking = false; // 表示回调队列是否包含当前帧
+let preIdx = 0;
+function adjustToc(scrollY, offset = 0) {
+  // 偏差值
+  tocParts[preIdx].classList.remove("selected");
+  let curIdx = 0;
+  for (; curIdx < sectionLen.length; curIdx++) {
+    if (scrollY + offset < sectionLen[curIdx]) {
+      break;
+    }
+  }
+  tocParts[curIdx].classList.add("selected");
+  preIdx = curIdx;
+}
+
+scrollThrottlingWrapper.addEventListener("scroll", (e) => {
+  const scrollY = e.target.scrollTop;
+  if (!ticking) {
+    requestAnimationFrame(() => {
+      adjustToc(scrollY, 40);
+      ticking = false;
+    });
+    ticking = true;
+  }
+});

408dca24c4c59532605b9ad508bedc9f1b30acfa
mangwu <1185956753@qq.com>
2024-09-04 17:08:34 +0800

    toDataURL

--- ProfessionalJavaScriptForWebDeveloper4/ch18 - Animation and Graphics with Canvas/18.2 基本画布功能（Basic Canvas Usage）/18.2.4 HTMLCanvasElement API/18.2.4.2 HTMLCanvasElement.toDataURL()/18.2.4.2 HTMLCanvasElement.toDataURL().js
+++ ProfessionalJavaScriptForWebDeveloper4/ch18 - Animation and Graphics with Canvas/18.2 基本画布功能（Basic Canvas Usage）/18.2.4 HTMLCanvasElement API/18.2.4.2 HTMLCanvasElement.toDataURL()/18.2.4.2 HTMLCanvasElement.toDataURL().js
@@ -3,11 +3,22 @@ import { downloadFileByBlob } from "../../../../../ScatteredRecord/Blob/blob.js"
 
 const canvas1 = document.querySelector("canvas#canvas1");
 const ctx = canvas1.getContext("2d");
-ctx.fillStyle = "blue";
+ctx.fillStyle = "#43ff84";
 ctx.fillRect(0, 0, 100, 100);
+ctx.fillStyle = "#4096ff";
+ctx.fillRect(0, 22, 100, 30);
+ctx.fillStyle = "#ffffff";
+ctx.font = "bold 13px serif";
+ctx.fillText("Hello, World!", 0, 42);
 var fullQuality = canvas1.toDataURL("image/jpeg", 1.0);
 var mediumQuality = canvas1.toDataURL("image/jpeg", 0.5);
 var lowQuality = canvas1.toDataURL("image/jpeg", 0.1);
+const lowImg = document.querySelector(".low-img");
+const midImg = document.querySelector(".mid-img");
+const highImg = document.querySelector(".high-img");
+lowImg.setAttribute("src", lowQuality);
+midImg.setAttribute("src", mediumQuality);
+highImg.setAttribute("src", fullQuality);
 console.log("fullQuality", fullQuality, fullQuality.length);
 // data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/4gHYSU...AA/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8AP/B//9k= 1035
 console.log("mediumQuality", mediumQuality, mediumQuality.length);
